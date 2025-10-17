289d2ada4fb90ada78c595cb16643e76dd44cc09
jgvnoriega-cmd <jgvnoriega@gmail.com>
2025-10-17 07:47:06 -0600

    Add files via upload

---
+++ Practicas S/Practica 20/js/index.js
@@ -0,0 +1,22 @@
+let nombre = "Johan Gael";
+let apellido = 'Vasquez Noriega'
+let nombrecompleto = nombre + ' ' + apellido;
+console.log(nombrecompleto);
+
+let numero = 250;
+let numero2 = 300;
+var numero3 = 100;
+numero4 = 50; //declaraciones antiguas
+console.log(numero,numero2,numero3,numero4)
+
+let suma = numero + numero2 + numero3 + numero4;
+console.log('El resultado de la suma es', suma);
+
+let resta = numero2 - numero3;
+console.log('El resultado de la resta es', resta);
+
+let multiplica = numero * numero2;
+console.log('El resultado de la multiplicación es', multiplica);
+
+let divide  = numero3 / numero4;
+console.log('El resultado de la división es', divide);
